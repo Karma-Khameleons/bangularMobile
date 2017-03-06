a512149d276ec4ebd9d12ad8449563dfb2a3b1be
LaDonna <boxgirl85@gmail.com>
2017-03-06 09:53:23 -0600

    updated app.js and created controller templates

---
+++ app/controllers/orderSuccessCtrl.js
@@ -0,0 +1,9 @@
+"use strict";
+
+app.controller("orderSuccess", function($scope, $location){
+
+
+	$scope.logout()=>{
+		
+	}
+});
---
+++ app/controllers/productSuccessCtrl.js
@@ -0,0 +1,5 @@
+"use strict"
+
+app.controller("productSuccess", function($scope, $location){
+	
+});
