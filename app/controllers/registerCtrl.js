c93402c26b9e0b88dee5763c339a41b814fa178e
Rico <richardthomaswhitfield@gmail.com>
2017-03-06 15:34:46 -0600

    adding register and login

--- app/controllers/registerCtrl.js
+++ app/controllers/registerCtrl.js
@@ -1,5 +1,36 @@
 "use strict";
 
-app.controller("register", function($scope, $location){
+app.controller("register", function($scope, $location, $http){
+
+	$scope.user = {
+	  username: "",
+	  password: "",
+	  email: "",
+	  first_name: "",
+	  last_name: ""
+	};
+
+	 $scope.register = function() {
+      $http({
+        url: "http://localhost:8000/register/",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        data: {
+          "username": $scope.user.username,
+          "password": $scope.user.password,
+          "email": $scope.user.email,
+          "first_name": $scope.user.first_name,
+          "last_name": $scope.user.last_name
+        }
+      }).then(res => {
+        if (res.data.success === true) {
+            $location.path('/');
+        } else {
+            console.log("error", error );
+        }
+      });
+  };
 	
 });
