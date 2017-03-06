d78d5f6dcc9e9b21ce339ffdc95818fcce88b79f
Rico <richardthomaswhitfield@gmail.com>
2017-03-06 11:55:20 -0600

    login and register templating setup

--- app/controllers/loginCtrl.js
+++ app/controllers/loginCtrl.js
@@ -1,5 +1,31 @@
 "use strict";
 
 app.controller("login", function($scope){
-	
+
+$scope.user = {
+	username: '',
+	password: ''
+};
+
+  $scope.login = function() {
+      $http({
+        url: "/login",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        data: {
+          "username": $scope.user.username,
+          "password": $scope.user.password
+        }
+      }).then(res => {
+        if (res.data.success === true) {
+            console.log(res)
+            $location.path('/');
+        } else {
+            // Show dialog element telling user that registration failed
+        }
+      });
+  };
+
 });
