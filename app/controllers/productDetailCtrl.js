ce211087975c2c7f01d2b4d69b8929f7200a99eb
Samuel Phillips <samphillips1879@gmail.com>
2017-03-06 14:30:07 -0600

    hard-coded getProduct on productDetailCtrl

--- app/controllers/productDetailCtrl.js
+++ app/controllers/productDetailCtrl.js
@@ -4,6 +4,6 @@ app.controller("productDetail", function($scope, productFactory, $routeParams){
 	$scope.title = 'Product Detail';
     
     // temporary hard_coding of product pk
-    $scope.product = productFactory.getProduct($routeParams.itemId);
-    // $scope.product = productFactory.getProduct(1);
+    $scope.product = productFactory.getProduct(1);
+    // $scope.product = productFactory.getProduct($routeParams.itemId);
 });
