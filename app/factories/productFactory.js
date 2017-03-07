5918be0532c3f618c67a3003d58aed041f01c97b
Samuel Phillips <samphillips1879@gmail.com>
2017-03-07 10:55:55 -0600

    product list retrieval working with RootFactory

--- app/factories/productFactory.js
+++ app/factories/productFactory.js
@@ -1,68 +1,115 @@
 "use strict";
 
-app.factory("productFactory", function($http, dbConf){
+app.factory("productFactory", function($http, RootFactory){
     let ProductFactory = {};
+
+    // let getRoot = ()=>{
+    //     RootFactory.getApiRoot()
+    //         .then((apiRoot) => {
+    //             console.log("apiRoot: ", apiRoot);
+    //             return apiRoot;
+    //             // api = apiRoot;
+    //         });
+    // };
+
 
     ProductFactory.getAllProducts = ()=>{
         // retrieves all products from the database
         return new Promise((resolve, reject)=>{
-            $http.get(`${dbConf.apiUrl}/products.json`)
-            .then((responseObject)=>{
-                let productsCollection = responseObject.data;
-                resolve(productsCollection);
+            RootFactory.getApiRoot()
+            .then((apiRoot)=>{
+                console.log("apiRoot: ", apiRoot);
+                $http({
+                    url: `${apiRoot.products}`,
+                    method: "GET"
+                    // headers: {
+                    //     "Content-Type": "application/x-www-form-urlencoded"
+                    // }
+                })
+                .then(res => {
+                    console.log("results:", res);
+                    resolve(res.data);
+                });
             });
         });
-    };
-
-    ProductFactory.getProduct = (pk)=>{
-        // retrieves a specific product from the database, queried via the product's primary key
-        return new Promise((resolve, reject)=>{
-            $http.get(`${dbConf.apiUrl}/products/${pk}.json`)
-            .then((responseObject)=>{
-                let product = responseObject.data;
-                resolve(product);
-            });
-        });
-    };
-
-    ProductFactory.createProduct = (newProduct)=>{
-        // saves a new product to the database
-        console.log("create a product");
-
-        $http({
-            url: `${dbConf.apiUrl}/create_product.json`,
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            data: newProduct
-        })
-        .then(res => {
-            console.log("results:", res);
-        });
-    };
-
-
-
-    ProductFactory.updateProduct = (updatedProduct)=>{
-        // saves changes to an existing product in the database
-        // accepts a product object and uses its pk attribute to target the updated product in the database
-        console.log("update product");
-
-        $http({
-            url: `${dbConf.apiUrl}/products/${updatedProduct.pk}.json`,
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            data: updatedProduct
-        })
-        .then(res => {
-            console.log("results:", res);
-        });
-
-
 
     };
+
+
+    // ProductFactory.getAllProducts = ()=>{
+    //     // retrieves all products from the database
+    //     return new Promise((resolve, reject)=>{
+    //         RootFactory.getApiRoot()
+    //             .then(
+    //                 root => 
+    //                     console.log("root: ", root);
+    //                     $http.get(`${root.products}`)
+    //                         .then(
+    //                             res => resolve(res.data.results)
+    //                             // res => $scope.product_types = res.data.results,
+    //                             // err => console.log
+    //                         )
+    //                 // ,err => console.log
+    //             );
+    //         // api = getRoot();
+    //         // console.log("api", api);
+    //         // $http.get(`${api.products}.json`)
+    //         // .then((responseObject)=>{
+    //         //     let productsCollection = responseObject.data;
+    //         //     resolve(productsCollection);
+    //         // });
+    //     });
+    // };
+
+    // ProductFactory.getProduct = (pk)=>{
+    //     // retrieves a specific product from the database, queried via the product's primary key
+    //     return new Promise((resolve, reject)=>{
+    //         $http.get(`${api}/products/${pk}.json`)
+    //         .then((responseObject)=>{
+    //             let product = responseObject.data;
+    //             resolve(product);
+    //         });
+    //     });
+    // };
+
+    // ProductFactory.createProduct = (newProduct)=>{
+    //     // saves a new product to the database
+    //     console.log("create a product");
+
+        // $http({
+        //     url: `${api}/create_product.json`,
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/x-www-form-urlencoded"
+        //     },
+        //     data: newProduct
+        // })
+        // .then(res => {
+        //     console.log("results:", res);
+        // });
+    // };
+
+
+
+    // ProductFactory.updateProduct = (updatedProduct)=>{
+    //     // saves changes to an existing product in the database
+    //     // accepts a product object and uses its pk attribute to target the updated product in the database
+    //     console.log("update product");
+
+    //     $http({
+    //         url: `${api}/products/${updatedProduct.pk}.json`,
+    //         method: "PUT",
+    //         headers: {
+    //             "Content-Type": "application/x-www-form-urlencoded"
+    //         },
+    //         data: updatedProduct
+    //     })
+    //     .then(res => {
+    //         console.log("results:", res);
+    //     });
+
+
+
+    // };
     return ProductFactory;
 });
