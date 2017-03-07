77364b98bbe606c6d64bb43723acf79c75ee0bce
Rico <richardthomaswhitfield@gmail.com>
2017-03-07 13:57:18 -0600

    reg and login complete

--- app/controllers/registerCtrl.js
+++ app/controllers/registerCtrl.js
@@ -15,6 +15,7 @@ app.controller("register", function($scope, $location, $http){
 	};
 
 	 $scope.register = function() {
+
       $http({
         url: "http://localhost:8000/register/",
         method: "POST",
