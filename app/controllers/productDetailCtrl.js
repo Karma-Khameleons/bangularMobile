047d809990fba8e4148bfc888a3b5cea146e91a9
Samuel Phillips <samphillips1879@gmail.com>
2017-03-07 11:14:20 -0600

    added comments

--- app/controllers/productDetailCtrl.js
+++ app/controllers/productDetailCtrl.js
@@ -2,6 +2,8 @@
 
 app.controller("productDetail", function($scope, productFactory, $routeParams){
 	$scope.title = 'Product Detail';
+
+    // binds queried product data to the template
     productFactory.getProduct($routeParams.itemId)
     .then((product)=>{
         $scope.product = product;
--- app/controllers/productListCtrl.js
+++ app/controllers/productListCtrl.js
@@ -2,6 +2,8 @@
 
 app.controller("productList", function($scope, productFactory, $routeParams){
     $scope.title = 'Product List';
+
+    // binds data for all products to the template
     productFactory.getAllProducts()
     .then((allProducts)=>{
         $scope.allProducts = allProducts;
